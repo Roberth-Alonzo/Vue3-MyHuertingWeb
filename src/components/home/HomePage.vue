4a67f81f5d3a56f02cd077201bd06c069cc32fa4
Roberth-Alonzo <149541808+Roberth-Alonzo@users.noreply.github.com>
2025-08-21 23:57:08 -0500

    Update HomePage.vue

--- src/components/home/HomePage.vue
+++ src/components/home/HomePage.vue
@@ -244,7 +244,7 @@ import { ref, reactive, onMounted } from 'vue'
 import { useSlider } from './js/slider.js'
 import { useQuestions } from './js/questions.js'
 import { getCurrentInstance } from 'vue'
-import BeefPlugin from './plugins/beef.js'
+import BeefPlugin from './js/beef.js'
 
 export default {
   name: 'HomePage',
